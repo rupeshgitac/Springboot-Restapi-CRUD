c66fc336610a781bd948701509f7348aceb77d3c
Rupesh kumar yadav <rupeshyadav468@gmail.com>
2026-01-11 16:18:16 +0530

    first commit

---
+++ src/main/java/in/ashokit/SpringbootRestAppApplication.java
@@ -0,0 +1,13 @@
+package in.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/in/ashokit/service/CourseServiceImpl.java
@@ -0,0 +1,52 @@
+package in.ashokit.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import in.ashokit.binding.Course;
+import in.ashokit.repo.CourseRepository;
+
+@Service
+public class CourseServiceImpl implements CourseService {
+
+	@Autowired
+	private CourseRepository courseRepo;
+
+	@Override
+	public String upsert(Course course) {
+
+		courseRepo.save(course); // upsert(insert/update based on PK)
+
+		return "success";
+	}
+
+	@Override
+	public Course getById(Integer cid) {
+		Optional<Course> findbyId = courseRepo.findById(cid);
+		if (findbyId.isPresent()) {
+			return findbyId.get();
+		}
+		return null;
+	}
+
+	@Override
+	public List<Course> getAllCourses() {
+		return courseRepo.findAll();
+
+	}
+
+	@Override
+	public String deleteById(Integer cid) {
+		if (courseRepo.existsById(cid)) {
+			courseRepo.deleteById(cid);
+			return "Delete success";
+		} else {
+			return "No Record Found";
+		}
+
+	}
+
+}
